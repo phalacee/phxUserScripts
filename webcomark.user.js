edefbe37b9fddaddcee777c80f3073f305d26e7f
Jason <phalacee@gmail.com>
2014-10-10 10:15:49 +0800

    Starting out

--- webcomark.user.js
+++ webcomark.user.js
@@ -1,1 +1,14 @@
-/* */
+/ ==UserScript==
+// @name        WebCoMark
+// @namespace   http://phalacee.com/userscripts/
+// @description Tracks webcomics for you
+// @version 1.0
+// @include     http://*cad-comic.com/cad/*
+// @include     http://*leasticoulddo.com/comic/*
+// @include     http://*gucomics.com/*
+// @include     http://*devilspanties.com/*
+// @include     http://*xkcd.com/*
+// @include     http://questionablecontent.net./view.php?comic=*
+// @include     http://*penny-arcade.com/comic/*
+// ==/UserScript==
+// hostname => pathname+query of last read comic strip
