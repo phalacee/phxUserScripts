e5f12de04138d2391e20945fdeba5dd49c06770a
Jason <phalacee@gmail.com>
2014-10-13 12:58:24 +0800

    Reddit User Script - Init

---
+++ reddit.user.js
@@ -0,0 +1,7 @@
+// ==UserScript==
+// @name        Imgur-on-reddit
+// @namespace   http://phalacee.com/userscripts/
+// @description Changes imgur links to the image instead of the page
+// @version 1.0
+// @include     http://*reddit.com/*
+// ==/UserScript==
