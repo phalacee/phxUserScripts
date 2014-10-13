d7a4356a91e31690d3aa8d5b77aa4c1b7aad3abc
Jason <phalacee@gmail.com>
2014-10-13 13:34:58 +0800

    Reddit User Script - image address swapping

--- reddit.user.js
+++ reddit.user.js
@@ -13,8 +13,18 @@ function init() {
 	var links = document.getElementsByTagName('a');
 	for (var i = links.length - 1; i >= 0; --i) {
 		href = links[i].getAttribute('href');
-		if (/\/imgur.com/i.test(href)) {
+
+		if (/imgur.com\/a/i.test(href)) { // Highlight albums
+			links[i].style.color = "#00CC99";
+		} else if (/imgur.com/i.test(href)) { // Highlight individual images
 			links[i].style.color = "#A6CF29";
+		}
+		// Swap to i.imgur.com
+		if (/\/imgur.com/i.test(href)) {
+			console.log(href);
+			newhref = href.replace("//imgur", "//i.imgur")  + ".jpg";
+			links[i].setAttribute('href', newhref);
+			console.log(newhref);
 		}
 	}
 }
